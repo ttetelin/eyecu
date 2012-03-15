469b21aed2574474a0ee368aceeeae8eef6821f8
khashi.xiong <khashi.xiong@gmail.com>
2012-03-15 04:00:59 +0000

    Updated XBEE UART code.

--- software/xbee/main.c
+++ software/xbee/main.c
@@ -2,119 +2,67 @@
 // Khashi Xiong
 // eyeCU
 // MSP430 UART HELLO WORLD
-//		Setup the UART for the MSP430 and allow for "Hello World" to be sent
-//		and received thru the UART ports.
+//		Have MSP430 UART communication
 /**********************************************************************************/
 
-#include "msp430x16x.h"
-#include <intrinsics.h>
-
-/*
-void portinit(void);
-void initdev(void);
-void pinconfig(void);
-void sendstring(char * str, int len);
-
-#define LED_ON                  P6OUT &= ~0x01; P6DIR |= 0x01;
-#define LED_OFF                 P6OUT |= 0x01;  P6DIR |= 0x01;
-#define LED_Check               (P6IN & 0x01)
-#define But_Check               (P6IN & 0x02)
-*/
-
-/**********************************************************************************/
-// Port Initialization on MSP430
-/**********************************************************************************/
-void portinit(void)
-{
-  //input_data=P1IN;          //Read only register
-  P1OUT = 0x00;               // The outputs are low
-  P1SEL = 0x00;               // Peripheral module function: I/O function is selected.
-  P1DIR = 0x00;               // 1 -> Output; 0 -> Input; All inputs
-  P1IES = 0x00;               // Interrupt Edge Select Registers
-  P1IE = 0x00;                // Disable all PORTx interrupts
-  P1IFG = 0x00;               // Clear all interrupt flags
-
-  //input_data=P2IN;          //Read only register
-  P2OUT = 0x00;               // The outputs are low
-  P2SEL = 0x00;               // Peripheral module function: I/O function is selected.
-  P2DIR = 0x00;               // 1 -> Output; 0 -> Input; All inputs
-  P2IES = 0x00;               // Interrupt Edge Select Registers
-  P2IE = 0x00;                // Disable all PORTx interrupts
-  P2IFG = 0x00;               // Clear all interrupt flags
-}
-
-/**********************************************************************************/
-// Initialize Devices on MSP430
-/**********************************************************************************/
-void initdev(void)
-{
-  // Disable interrupts during initialization process
-  _BIC_SR(GIE);
-
-  // Call Ports_Initialization
-  portinit();
-
-  // Timer_A
-  TACTL = 0x0004;  	 // Timer_A clear
-  TACCTL0 = 0x0010;	 // Timer_A Capture/compare interrupt enable
-  TACCR0 = 0x005F; 	 // Set TACCR0 value
-  TACTL = 0x0110;  	 // Selected: ACLK, No divider, Up mode
-
-  // Global Interrupt enabled
-  _BIS_SR(GIE);
-}
-
-/**********************************************************************************/
-// Configure UART on MSP430
-/**********************************************************************************/
-void uartconfig(void)
-{
-	  // UART Configuration
-	  P3SEL |= 0x60;						// P3.6,7 = USART1 TXD/RXD
-	  ME2 |= UTXE1 + URXE1;					// Enable USART1 TXD/RXD
-	  UCTL1 |= CHAR;						// 8-bit character
-	  UTCTL1 |= SSEL0;						// UCLK = ACLK = 32.768kHz
-	  UBR01 = 0x03;							// 32.768kHz/9600 - 3.41
-	  UBR11 = 0x00;
-	  UMCTL1 = 0x4a;						// Modulation
-	  UCTL1 &= ~SWRST;						// Initialize USART state machive
-	  IE2 |= URXIE1 + UTXIE1;				// Enable USART1 RX/TX interrupt
-	  while (!(IFG2 & UTXIFG1));			// Check if UART TX buffer is ready
-	  TXBUF0 = 0x41;
-	  while (!(IFG2 & UTXIFG1));			// Check if UART TX buffer is ready
-	  TXBUF1 = 0x41;
-}
-
-/**********************************************************************************/
-// Send string function
-/**********************************************************************************/
-void sendstring(char str[], int len)
-{
-	int i=0;
-	for(i=0; i<len; i++)
-	{
-		while (!(IFG2 & UTXIFG1));
-		TXBUF0 = str[i];
-	}
-}
+#include  "msp430x16x.h"
 
 void main(void)
 {
-  WDTCTL = WDTPW + WDTHOLD;             // Stop Watchdog Timer
+   WDTCTL = WDTPW + WDTHOLD;   // Stop WDT
+   BCSCTL1 = CALBC1_1MHZ;      // Set DCO
+   DCOCTL = CALDCO_1MHZ;
 
-  uartconfig();
-  initdev();
-  sendstring("Hello World\n",12);
+   P1SEL = 0x00;
+   P2SEL = 0x00;
+   P3SEL = 0x30;   //P3.4,5 = USCI_A0 TXD/RXD
+
+   P1DIR = 0x00;   //set columns P1.0-P1.3 as inputs
+   P2DIR = 0x39;   //set rows P2.3 and P2.4, P2.0 for # pressed = LED on, and P2.5 Xbee0 DI0 as outputs
+   P3DIR = 0xC0;   //set rows P3.6 and P3.7 as outputs
+   P4DIR = 0x10;   //set P4.6 as fancontrol: fancontrol=0 (off); fancontrol=1 (on)
+
+   P3OUT = 0x00;   //initialize output port to ground
+   P2OUT = 0x00;
+
+   __delay_cycles(100000);
+   lcd_init();      //get the lcd booted up
+
+
+  //RX Parameters
+  UCA0CTL1 |= UCSSEL_2;                     // SMCLK
+  UCA0BR0 = 104;                            // 1MHz 9600
+  UCA0BR1 = 0;                              // 1MHz 9600
+  UCA0MCTL = UCBRS0;                        // Modulation UCBRSx = 1
+  UCA0CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
+  IE2 |= UCA0RXIE;                          // Enable USCI_A0 RX interrupt
+
+  while (1) {
+     __bis_SR_register(CPUOFF + GIE);       // Enter LPM0 w/ interrupt
+     displayTemp();
+     //inputTemp();
+  }
 }
 
-/**********************************************************************************/
-// Interrupt to receive bits
-/**********************************************************************************/
-/*
-#pragma vector=USART1RX_VECTOR
-__interrupt void usart1_rx (void)
+// Receive RXed character
+#pragma vector=USCIAB0RX_VECTOR
+__interrupt void USCI0RX_ISR(void)
 {
-	while (!(IFG2 & UTXIFG1));
-	TXBUF1 = RXBUF1;
+
+     apiframe[j] = UCA0RXBUF;
+     if ((j == 0 && apiframe[j] == 0x7E) || j > 0) {
+         data[j] = apiframe[j];
+     }
+     if (j == 11) {
+        dh = data[j];
+     }
+     if (j == 12) {
+        dl = data[j];
+             //dataReady = 1;
+     }
+     if (j > sizeof apiframe - 1) {
+          j = 0;
+          __bic_SR_register_on_exit(CPUOFF);        // Return to active mode
+     }
+     j++;
 }
-*/
